fba7d6eb8157fd840ecc87eae6a381bbf8f3aebf
KAI_Monmoy <aftarulislam@gmail.com>
2019-11-12 05:36:10 +0600

    :sparkles: Implemented News API Provider

---
+++ lib/src/resources/news_api_provider.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:flutter_hacker_news/src/models/item_model.dart';
+import 'package:http/http.dart' show Client;
+
+const _kBaseURL = 'https://hacker-news.firebaseio.com/v0';
+
+class NewsApiProvider {
+  final client = Client();
+
+  fetchTopIds() async {
+    final response = await client.get('$_kBaseURL/topstories.json');
+    final ids = json.decode(response.body);
+
+    return ids;
+  }
+
+  fetchItem(int id) async {
+    final response = await client.get('$_kBaseURL/item/$id.json');
+    final parsedJson = json.decode(response.body);
+
+    return ItemModel.fromJson(parsedJson);
+  }
+}
