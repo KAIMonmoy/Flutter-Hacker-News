82b58199c461e84dfe5fe6877b5d00944797c6f1
KAI_Monmoy <aftarulislam@gmail.com>
2019-11-13 01:41:23 +0600

    :sparkles: Added Navigation

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'blocs/stories_provider.dart';
 import 'screens/news_list.dart';
+import 'screens/news_detail.dart';
 
 class App extends StatelessWidget {
   @override
@@ -10,14 +11,25 @@ class App extends StatelessWidget {
       child: MaterialApp(
         title: 'Hacker News',
         theme: ThemeData.dark(),
-        onGenerateRoute: (RouteSettings settings) {
-          return MaterialPageRoute(
-            builder: (context) {
-              return NewsList();
-            },
-          );
-        },
+        onGenerateRoute: routes,
       ),
     );
+  }
+
+  Route routes(RouteSettings settings) {
+    if (settings.name == '/') {
+      return MaterialPageRoute(
+        builder: (context) {
+          return NewsList();
+        },
+      );
+    } else {
+      return MaterialPageRoute(
+        builder: (context) {
+          final itemId = int.parse(settings.name.replaceFirst('/', ''));
+          return NewsDetail(itemId: itemId);
+        },
+      );
+    }
   }
 }
--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
 class NewsDetail extends StatelessWidget {
+  final int itemId;
+  NewsDetail({this.itemId});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('News Detail!!!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Story Detail',
+          style: TextStyle(
+            color: Colors.redAccent,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('$itemId News Detail!!!'),
+      ),
     );
   }
 }
