b2e3f3f3fdbae4e05c274e919f1ae182f7c6cde4
KAI_Monmoy <aftarulislam@gmail.com>
2019-11-12 20:38:04 +0600

    :sparkles: Handled Item Backend Part

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,20 +1,42 @@
 import 'package:rxdart/rxdart.dart';
 
+import '../models/item_model.dart';
 import '../resources/repository.dart';
 
 class StoriesBloc {
   final _repository = Repository();
 
   final _topIds = PublishSubject<List<int>>();
+  final _items = BehaviorSubject<int>();
+  Observable<Map<int, Future<ItemModel>>> items;
 
+  StoriesBloc() {
+    items = _items.stream.transform(_itemsTransformer());
+  }
+
+  // Getter to Streams
   Observable<List<int>> get topIds => _topIds.stream;
+
+  // Getter to Sinks
+  Function(int) get fetchItem => _items.sink.add;
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
     _topIds.sink.add(ids);
   }
 
+  _itemsTransformer() {
+    return ScanStreamTransformer(
+      (Map<int, Future<ItemModel>> cache, int id, _) {
+        cache[id] = _repository.fetchItem(id);
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
+    );
+  }
+
   void dispose() {
     _topIds.close();
+    _items.close();
   }
 }
